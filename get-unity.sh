ef17c12cbf3c2e0e8b3560731f309e93ec25e675
kevinta893 <kevinta893@gmail.com>
2018-03-14 15:20:12 -0600

    Revert "Show download progress"
    This reverts commit 925d86fae754b38763a9275b896b6b451f4f432a.

--- get-unity.sh
+++ get-unity.sh
@@ -3,5 +3,5 @@ URL=http://download.unity3d.com/download_unity/linux/
 PKG=unity-editor-5.4.0p1+20160810_amd64.deb
 
 echo "Downloading Unity3D installer..."
-curl -o /app/unity_editor.deb "${URL}${PKG}" 
+curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
 echo "Unity3D installer downloaded."
