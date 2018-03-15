f643d4e6308bf2b21326537e731649f2a8c29417
kevinta893 <kevinta893@gmail.com>
2018-03-15 16:34:23 -0600

    Show echo on download

--- get-unity.sh
+++ get-unity.sh
@@ -3,5 +3,5 @@ URL=http://beta.unity3d.com/download/ee86734cf592/
 PKG=unity-editor_amd64-2017.2.0f3.deb
 
 echo "Downloading Unity3D installer..."
-curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
+curl -o /app/unity_editor.deb "${URL}${PKG}" 
 echo "Unity3D installer downloaded."
