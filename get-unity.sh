159e942ed24556e424490b1615f55695db4863d5
kevinta893 <kevinta893@gmail.com>
2018-05-24 23:12:24 -0600

    Added unity installer for 2018
    Example of how to install Unity commandline only

--- get-unity.sh
+++ get-unity.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-URL=http://download.unity3d.com/download_unity/linux/
-PKG=unity-editor-5.4.0p1+20160810_amd64.deb
+URL=https://beta.unity3d.com/download/170f0691b973/
+PKG=UnitySetup-2018.1.0f2
+INSTALL_LOCATION=~/Unity
 
-echo "Downloading Unity3D installer..."
-curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
-echo "Unity3D installer downloaded."
+#install unity and the installer's dependancies
+sudo apt-get -y update
+sudo apt-get -y install libgtk2.0-0
+sudo apt-get -y install libsoup2.4-1
+sudo apt-get -y install libarchive13
+sudo apt-get -y install libpng12
+sudo apt-get -y install libgconf-2-4
+wget "$URL/$PKG"
+chmod +x $PKG
+mkdir -p $INSTALL_LOCATION
+yes | ./$PKG --unattended --install-location=$INSTALL_LOCATION
+
+
